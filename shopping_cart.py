e8dda88ec17174436b6275466e45e44adcd955f5
Andrew Capaldo <andycapaldo2@gmail.com>
2023-09-29 15:09:13 -0600

    95% done with project

--- shopping_cart.py
+++ shopping_cart.py
@@ -15,11 +15,11 @@ def clear_output():
 shopping_list = {}
 total_price = 0
 
-def total(shopping_list):
+def total(shopping_list=shopping_list):
     total_price = 0
     for k, v in shopping_list.items():
         total_price += (v[0] * v[1])
-    print(f'Total: {total_price}')
+    print(f'Total: ${round(total_price, 2)}')
 
 
 def add_to_cart():
@@ -44,22 +44,50 @@ def add_to_cart():
             quantity_input = input('How much of this product would you like to add?\n\t')
             try:
                 quantity = int(quantity_input)
-                break # Break the loop if conversion to float is successful
+                break # Break the loop if conversion to int is successful
             except ValueError:
                 print('Please only enter a whole number.')
-
     if product in shopping_list:
-        shopping_list[product][1] += quantity
+        shopping_list[product][1] += quantity # No need to add a new key, just update the quantity 
     else:
-        shopping_list[product] = [price, quantity]
-    print(f'Success! You have added {quantity} {product} to your cart.')
+        shopping_list[product] = [price, quantity] # A new key:value pair is set up if it doesn't already exist
+    print(f'\nYou have added {quantity} {product} to your cart.\n')
     print('Items in Cart:')
     for k, v in shopping_list.items():
-        print(f'\t{v[1]} {k} {v[0] * v[1]}')
+        print(f'\t{v[1]} {k} ${round(v[0] * v[1], 2)}')
     total(shopping_list)
 
+
 def remove_from_cart():
-    item_to_remove = input('What would you like to remove from your cart?\n')
+    item_to_remove = input('What would you like to remove from your cart?\n\t').title()
+    if item_to_remove not in shopping_list:
+        print("\nThat item is not in your cart.")
+    elif item_to_remove in shopping_list and shopping_list[item_to_remove][1] == 1:
+            del shopping_list[item_to_remove]
+            print(f'{item_to_remove} has been removed from your cart.\n')
+            print('Items in Cart:')
+            for k, v in shopping_list.items():
+                print(f'\t{v[1]} {k} ${round(v[0] * v[1], 2)}')
+            return total(shopping_list)
+    else:
+        while True:
+            quantity_to_remove_input = input(f'How many {item_to_remove} would you like to remove?\n\t')
+            try:
+                quantity_to_remove = int(quantity_to_remove_input)
+                break # Break the loop if conversion to int is successful
+            except ValueError:
+                print('Please only enter a whole number.')
+    for k, v in shopping_list.items():
+        if k == item_to_remove:
+            shopping_list[k][1] -= quantity_to_remove
+            print(f'\n{quantity_to_remove} {item_to_remove} have been removed from your cart.\n')
+    if shopping_list[item_to_remove][1] == 0:
+        del shopping_list[item_to_remove]
+    print('Items in Cart:')
+    for k, v in shopping_list.items():
+        print(f'\t{v[1]} {k} ${round(v[0] * v[1], 2)}')
+    total(shopping_list)
+
 
 
 
@@ -89,3 +117,14 @@ def shopping_cart():
 
 
 shopping_cart()
+
+
+# shopping_cart_1 = {'Apples': [2.99, 4], 'Bananas': [1.99, 3]}
+# new_quantity = 0
+# quantity_to_remove = 3
+
+# for k, v in shopping_cart_1.items():
+#     shopping_cart_1[k][1] -= quantity_to_remove 
+
+# print(shopping_cart_1)
+
